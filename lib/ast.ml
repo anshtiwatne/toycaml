5262dad10bf4723d3e2475f77e912545212ca2e1
Docker <docker@example.com>
2025-12-30 14:38:24 +0000

    improve naming

--- lib/ast.ml
+++ lib/ast.ml
@@ -1,19 +1,49 @@
-type ty = Bool | Int | Arrow of ty * ty | Product of ty * ty
-type con = BCon of bool | ICon of int
-type var = string
-type op = Add | Sub | Mul | Leq
+type id = string
+type ty = TInt | TBool | TArrow of ty * ty | TPair of ty * ty
+type unop = Neg | Not
+
+type binop =
+  (* Arithmetic *)
+  | Add
+  | Sub
+  | Mul
+  (* Comparison *)
+  | Eq
+  | Neq
+  | Lt
+  | Gt
+  | Leq
+  (* Logical (Lazy) *)
+  | And
+  | Or
+
+type const = ICon of int | BCon of bool
 
 type exp =
-  | Con of con
-  | Var of var
+  (* Atoms *)
+  | Var of id
+  | Const of const
+  (* Operations *)
+  | UnOp of unop * exp
+  | BinOp of binop * exp * exp
+  (* Control Flow *)
   | If of exp * exp * exp
-  | OApp of op * exp * exp
-  | Fun of var * ty * exp
-  | RFun of var * var * ty * ty * exp
-  | FApp of exp * exp
+  (* Pairs *)
   | Pair of exp * exp
   | Fst of exp
   | Snd of exp
+  (* Functions *)
+  | App of exp * exp
+  | Fun of id * ty * exp
+  | RFun of id * id * ty * ty * exp
+  (* Bindings *)
+  | Let of id * exp * exp
+  | LetRec of id * id * ty * ty * exp * exp
+
+(* Slight design inconsistency:
+RFun/LetRec require some types that Fun/Let do not and can infer.
+I could skip inference and require types everywhere but that would make elaboration less interesting.
+Maybe eventually I'll work on implementing type inference for RFun/LetRec also *)
 
 (* type token =
   | LP
