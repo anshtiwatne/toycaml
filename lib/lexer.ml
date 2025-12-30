ab3322bb8548d30fccbbdea6b8052727c82efe81
Docker <docker@example.com>
2025-12-30 19:57:22 +0000

    implement lexer

---
+++ lib/lexer.ml
@@ -0,0 +1,86 @@
+open Token
+
+let lex_err msg = failwith ("Lexical error: " ^ msg)
+
+(* Character predicates *)
+let is_digit = function '0' .. '9' -> true | _ -> false
+
+let is_id_char = function
+  | 'a' .. 'z' | 'A' .. 'Z' | '_' | '0' .. '9' -> true
+  | _ -> false
+
+(* Utility functions *)
+let int_of_digit c = Char.code c - Char.code '0'
+let explode s = List.init (String.length s) (String.get s)
+let implode s = String.of_seq (List.to_seq s)
+
+(* Keyword lookup *)
+let keyword_or_var = function
+  (* Type constructors *)
+  | "int" -> INT
+  | "bool" -> BOOL
+  (* Control flow *)
+  | "if" -> IF
+  | "then" -> THEN
+  | "else" -> ELSE
+  (* Pairs *)
+  | "fst" -> FST
+  | "snd" -> SND
+  (* Functions *)
+  | "fun" -> FUN
+  | "rfun" -> RFUN
+  (* Bindings *)
+  | "let" -> LET
+  | "rec" -> REC
+  | "in" -> IN
+  (* Constants *)
+  | "true" -> CON (BCon true)
+  | "false" -> CON (BCon false)
+  (* Variables *)
+  | s -> VAR s
+
+(* Main lexer *)
+let rec lex cs =
+  match cs with
+  | [] -> []
+  (* Whitespace *)
+  | (' ' | '\t' | '\n') :: cr -> lex cr
+  (* Delimiters *)
+  | '(' :: cr -> LP :: lex cr
+  | ')' :: cr -> RP :: lex cr
+  | ',' :: cr -> COMMA :: lex cr
+  | ':' :: cr -> COL :: lex cr
+  (* Type constructors *)
+  | '-' :: '>' :: cr -> ARR :: lex cr
+  (* Unary operators *)
+  | '-' :: cr -> SUB :: lex cr  (* negation and subtraction *)
+  | '!' :: cr -> NOT :: lex cr
+  (* Binary operators: arithmetic *)
+  | '+' :: cr -> ADD :: lex cr
+  | '*' :: cr -> MUL :: lex cr
+  (* Binary operators: comparison *)
+  | '=' :: cr -> EQ :: lex cr
+  | '<' :: '>' :: cr -> NEQ :: lex cr
+  | '<' :: cr -> LT :: lex cr
+  | '>' :: cr -> GT :: lex cr
+  (* Binary operators: logical *)
+  | '&' :: '&' :: cr -> AND :: lex cr
+  | '|' :: '|' :: cr -> OR :: lex cr
+  (* Numbers *)
+  | '0' .. '9' :: _ -> lex_num 0 cs
+  (* Identifiers and keywords *)
+  | ('a' .. 'z' | 'A' .. 'Z' | '_') :: _ -> lex_var [] cs
+  | c :: _ -> lex_err ("Unexpected character '" ^ String.make 1 c ^ "'")
+
+(* Lex integer literals *)
+and lex_num acc = function
+  | c :: cr when is_digit c -> lex_num ((acc * 10) + int_of_digit c) cr
+  | cs -> CON (ICon acc) :: lex cs
+
+(* Lex identifiers and keywords *)
+and lex_var acc cs =
+  match cs with
+  | c :: cr when is_id_char c -> lex_var (c :: acc) cr
+  | _ ->
+      let s = implode (List.rev acc) in
+      keyword_or_var s :: lex cs
