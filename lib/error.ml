f441a8469c9f20ce11a3759dc65bd90ab92fbb79
Docker <docker@example.com>
2026-01-04 21:21:41 +0000

    minor change

--- lib/error.ml
+++ lib/error.ml
@@ -3,7 +3,7 @@ exception TypeError of string
 exception RuntimeError of string
 
 let string_of_exception = function
-  | SyntaxError msg -> "Syntax Error: " ^ msg
-  | TypeError msg -> "Type Error: " ^ msg
-  | RuntimeError msg -> "Runtime Error: " ^ msg
+  | SyntaxError msg -> "SyntaxError: " ^ msg
+  | TypeError msg -> "TypeError: " ^ msg
+  | RuntimeError msg -> "RuntimeError: " ^ msg
   | e -> Printexc.to_string e
